b884e222f054f8f07bef86911f4cba21cda6da87
gergo091 <gergo0912@gmail.com>
2017-01-20 20:00:45 +0100

    finished challenge persistent bugger

--- PersistentBugger.py
+++ PersistentBugger.py
@@ -1,22 +1,27 @@
-# Write a function that counts how many different ways you can make change for an amount of money, given an array of coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2:
-
+# Write a function that counts how many different ways you can make change for an amount of money, 
+# given an array of coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2:
 # 1+1+1+1, 1+1+2, 2+2.
-# The order of coins does not matter:
 
+# The order of coins does not matter:
 # 1+1+2 == 2+1+1
 # Also, assume that you have an infinite ammount of coins.
 
 # Your function should take an amount to change and an array of unique denominations for the coins:
-
 #   count_change(4, [1,2]) # => 3
 #   count_change(10, [5,2,3]) # => 4
 #   count_change(11, [5,7]) # => 0
 
 def count_change(money, coins):
-    # your implementation here
+    ways = [1]+[0]*money
+    for coin in coins:
+        for i in range(coin, money+1):
+            ways[i]+=ways[i-coin]
+    return ways[money]
 
 if __name__ == '__main__':
     print count_change(4, [1,2])
+    print count_change(10, [5,2,3])
+    print count_change(11, [5,7])
     # test.assert_equals(3, count_change(4, [1,2]))
     # test.assert_equals(4, count_change(10, [5,2,3]))
     # test.assert_equals(0, count_change(11, [5,7]))
