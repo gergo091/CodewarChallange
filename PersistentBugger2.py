0d313199d33429e416794c82fef9b9f457999816
Gergely Czakó <gergely.czako@hq.eset.com>
2017-02-18 20:55:19 +0100

    added new resolved challenge

---
+++ PersistentBugger2.py
@@ -0,0 +1,49 @@
+# Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+# For example:
+
+#  persistence(39) => 3  # Because 3*9 = 27, 2*7 = 14, 1*4=4
+#                        # and 4 has only one digit.
+
+#  persistence(999) => 4 # Because 9*9*9 = 729, 7*2*9 = 126,
+#                        # 1*2*6 = 12, and finally 1*2 = 2.
+
+#  persistence(4) => 0   # Because 4 is already a one-digit number.
+#  persistence(39) # returns 3, because 3*9=27, 2*7=14, 1*4=4
+#                  # and 4 has only one digit
+
+#  persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+#                   # 1*2*6=12, and finally 1*2=2
+
+#  persistence(4) # returns 0, because 4 is already a one-digit number
+
+def persistence(n):
+    digits = [int(x) for x in str(n)]
+    count = 0
+    tmp = 1
+    end = False
+    if len(digits) == 1:
+        return count
+    else:
+        while end == False:
+            for i in range(0, len(digits), 1):
+                tmp *= digits[i]
+            digits = [int(x) for x in str(tmp)]
+            if len(digits) == 1:
+                end = True
+            count += 1
+            tmp = 1
+        return count
+
+
+if __name__ == '__main__':
+    print persistence(39)
+    print persistence(4)
+    print persistence(25)
+    print persistence(999)
+
+# Test.it("Basic tests")
+# Test.assert_equals(persistence(39), 3)
+# Test.assert_equals(persistence(4), 0)
+# Test.assert_equals(persistence(25), 2)
+# Test.assert_equals(persistence(999), 4)
