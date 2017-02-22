70d8c8b2f37e42e3f47f224ee30f989472e01079
Gergely Czakó <gergely.czako@hq.eset.com>
2017-02-22 12:44:06 +0100

    deleted some lines

--- PersistentBugger.py
+++ PersistentBugger.py
@@ -22,6 +22,3 @@ if __name__ == '__main__':
     print count_change(4, [1,2])
     print count_change(10, [5,2,3])
     print count_change(11, [5,7])
-    # test.assert_equals(3, count_change(4, [1,2]))
-    # test.assert_equals(4, count_change(10, [5,2,3]))
-    # test.assert_equals(0, count_change(11, [5,7]))
